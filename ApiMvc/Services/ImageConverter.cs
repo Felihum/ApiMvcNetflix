d453ce17cf91723cbef07b287c9367e53bc49f88
Felihum <felipemedeirosinfo@gmail.com>
2024-07-08 15:59:07 -0300

    Image Working

---
+++ ApiMvc/Services/ImageConverter.cs
@@ -0,0 +1,21 @@
+﻿namespace ApiMvc.Services
+{
+    public class ImageConverter
+    {
+        public static string ConvertFromImageToBase(string imagePath)
+        {
+            try
+            {
+                byte[] imageBytes = File.ReadAllBytes(imagePath);
+
+                string baseString = Convert.ToBase64String(imageBytes);
+
+                return baseString;
+            }
+            catch (Exception error)
+            {
+                throw error;
+            }
+        }
+    }
+}
